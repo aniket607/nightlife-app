d6299f55d5a95372bfee3af72ea80300bb9259cd
aniket607 <aniketgoyal607@gmail.com>
2025-01-16 11:55:12 +0530

    bug fixes

--- auth.ts
+++ auth.ts
@@ -12,10 +12,17 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       console.log("User signed in");
       return true
     },
+    redirect({ url, baseUrl }) {
+      // If the url is relative, prefix it with the base url
+      if (url.startsWith('/')) return `${baseUrl}${url}`
+      // If the url is absolute but on the same host, return it
+      else if (new URL(url).origin === baseUrl) return url
+      // Default to the organizer dashboard
+      return `${baseUrl}/organizer`
+    }
   },
 
   pages: {
     signIn: "/login",
   }
 })
-
